6f976900030d6b6ca85d4ad62a449c61447a408f
gabrieldhofer <leirbagabriel468@gmail.com>
2025-04-25 12:28:25 -0500

    gg

--- main.py
+++ main.py
@@ -3,25 +3,42 @@ import json
 import time
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
+from pyspark.sql.functions import array_contains
+import re
 
-URL = "http://data.cms.gov/provider-data/api/1/metastore/schemas/dataset/items"
-response = requests.get(URL)
+def get_data() -> list:
+  """ Retrieve data via HTTP GET request """
+  URL = "http://data.cms.gov/provider-data/api/1/metastore/schemas/dataset/items"
+  response = requests.get(URL)
+  data_dict = {}
+  if response.status_code == 200:
+      try:
+          return response.json()
+      except ValueError:
+          print("Response is not in JSON format")
+  else:
+      print(f"Request failed with status code {response.status.code}")
 
-data_dict = {}
-if response.status_code == 200:
-    try:
-        data_dict = response.json()
-    except ValueError:
-        print("Response is not in JSON format")
-else:
-    print(f"Request failed with status code {response.status.code}")
 
-#print(json.dumps(data_dict, indent=4))
+def filter_by_hospitals_theme(df) -> list:
+  """ only return rows with theme containing 'Hospitals' """
+  return df.filter(array_contains(df.theme, 'Hospitals'))
 
-for obj in data_dict:
-    print(json.dumps(obj, indent=2))
-    theme = obj['theme'][0]
-    print(f"THEME: {theme}")
-    time.sleep(1)
-    # do something
-    
+
+def cols_to_snake_case(df) -> None:
+  """ convert column names to snake case """
+  for col in df.columns:
+      new_col = re.sub(r"(?<!^)(?=[A-Z])", "_", col).lower()
+      df = df.withColumnRenamed(col, new_col)
+  return df
+
+
+def main():
+  spark = SparkSession.builder.getOrCreate()
+  df = spark.createDataFrame(get_data())
+  filtered = filter_by_hospitals_theme(df)
+  case_converted = cols_to_snake_case(filtered)
+
+  case_converted.show()
+
+main()
