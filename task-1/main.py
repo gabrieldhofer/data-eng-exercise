fdc431abae48abca8b7ea7f36b1bcc5a5f7f8263
gabrieldhofer <leirbagabriel468@gmail.com>
2025-04-28 18:55:18 -0500

    gg

--- task-1/main.py
+++ task-1/main.py
@@ -6,6 +6,61 @@ from pyspark.sql import Row
 from pyspark.sql.functions import array_contains
 from pyspark.sql import DataFrame
 import re
+from pyspark.sql.functions import current_timestamp
+import schedule
+from pytz import timezone
+import datetime
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType, MapType, BooleanType
+
+
+cols = [
+    "@type", "access_level", "bureau_code", "contact_point", "description", 
+    "distribution", "identifier", "issued", "keyword", "landing_page", 
+    "modified", "program_code", "publisher", "released", "theme", "title", 
+    "archive_exclude", "next_update_date"
+]
+
+schema = StructType([
+    StructField('@type', StringType(), True), 
+    StructField('access_level', StringType(), True), 
+    StructField('bureau_code', StringType(), True), 
+    StructField('contact_point', StringType(), True), 
+    StructField('description', StringType(), True), 
+    StructField('distribution', StringType(), True), 
+    StructField('identifier', StringType(), True), 
+    StructField('issued', StringType(), True), 
+    StructField('keyword', StringType(), True), 
+    StructField('landing_page', StringType(), True), 
+    StructField('modified', StringType(), True), 
+    StructField('program_code', StringType(), True), 
+    StructField('publisher', StringType(), True), 
+    StructField('released', StringType(), True), 
+    StructField('theme', StringType(), True), 
+    StructField('title', StringType(), True), 
+    StructField('archive_exclude', BooleanType(), True), 
+    StructField('next_update_date', StringType(), True)
+])
+
+schema2 = StructType([
+    StructField('@type', StringType(), True), 
+    StructField('accessLevel', StringType(), True), 
+    StructField('bureauCode', StringType(), True), 
+    StructField('contactPoint', StringType(), True), 
+    StructField('description', StringType(), True), 
+    StructField('distribution', StringType(), True), 
+    StructField('identifier', StringType(), True), 
+    StructField('issued', StringType(), True), 
+    StructField('keyword', StringType(), True), 
+    StructField('landingPage', StringType(), True), 
+    StructField('modified', StringType(), True), 
+    StructField('programCode', StringType(), True), 
+    StructField('publisher', StringType(), True), 
+    StructField('released', StringType(), True), 
+    StructField('theme', StringType(), True), 
+    StructField('title', StringType(), True), 
+    StructField('archiveExclude', BooleanType(), True), 
+    StructField('nextUpdateDate', StringType(), True)
+])
 
 def get_data() -> list:
   """ Retrieve data via HTTP GET request """
@@ -13,12 +68,12 @@ def get_data() -> list:
   response = requests.get(URL)
   data_dict = {}
   if response.status_code == 200:
-    try:
-      return response.json()
-    except ValueError:
-      print("Response is not in JSON format")
+      try:
+          return response.json()
+      except ValueError:
+          print("Response is not in JSON format")
   else:
-    print(f"Request failed with status code {response.status.code}")
+      print(f"Request failed with status code {response.status.code}")
 
 
 def filter_by_hospitals_theme(df) -> list:
@@ -29,54 +84,98 @@ def filter_by_hospitals_theme(df) -> list:
 def cols_to_snake_case(df) -> None:
   """ convert column names to snake case """
   for col in df.columns:
-    new_col = re.sub(r"(?<!^)(?=[A-Z])", "_", col).lower()
-    df = df.withColumnRenamed(col, new_col)
+      new_col = re.sub(r"(?<!^)(?=[A-Z])", "_", col).lower()
+      df = df.withColumnRenamed(col, new_col)
   return df
 
 
-def job():
-  spark = SparkSession.builder.getOrCreate()
-  df = spark.createDataFrame(get_data())
-  filtered = filter_by_hospitals_theme(df)
-  case_converted = cols_to_snake_case(filtered)
-  case_converted.show()
-
-# https://schedule.readthedocs.io/en/stable/examples.html
-# https://schedule.readthedocs.io/en/stable/timezones.html
-
-import schedule
-from pytz import timezone
-import datetime
-
-#schedule.every(5).seconds.do(job)
-
-def main():
-  schedule.every().day.at("12:11:00", timezone("America/Chicago")).do(job)
-  while True:
-    schedule.run_pending()
-    time.sleep(30)
-
-def main2(data_location="data.csv"):
-
-  # load dataframe from file
-  df = spark.read.csv(
+def read_tgt_df(spark, data_location="data.csv"):
+  return spark.read.csv(
     data_location, 
     header=True, 
-    inferSchema=True
+    schema=schema
   )
+  
 
-  # get new data every minute
-  schedule.every(1).minute.at("8:20:00", timezone("America/Chicago")).do(job)
-  while True:
-    schedule.run_pending()
-    time.sleep(1)
 
-  # write updated dataframe to the same file 
-  df.write.csv(
+def write_tgt_df(tgt_df, data_location="data.csv"):
+  tgt_df.write.csv(
     data_location, 
     header=True, 
     mode="overwrite"
   )
+
+
+def upsert(tgt_df, src_df):
+
+  # Perform CDC: Identify new rows
+  cdc_updates = src_df.alias("src")\
+    .join(
+        tgt_df.alias("tgt"), 
+        on="identifier", 
+        how="left_anti"
+    )    
+
+  # Identify updates
+  cdc_existing = src_df.alias("src")\
+    .join(tgt_df.alias("tgt"), on="identifier", how="inner")
+  cdc_existing.show()
+  #  .filter(col("src.modified") > col("tgt.modified"))\
+  #  .withColumn("current_tsp", current_timestamp())  # Update timestamp for updates
+
+  # Keep rows that weren't in the new df (no deletions)
+  cdc_retain = tgt_df.alias("tgt").join(src_df.alias("src"), on="identifier", how="left_anti")
+
+  cdc_updates.show()
+  cdc_existing.show()
+  cdc_retain.show()
+  
+  src_cols = ["src." + x for x in cols]
+  tgt_cols = ["tgt." + x for x in cols]
+  
+  # Align schemas
+  cdc_updates = cdc_updates.select(*src_cols)
+  cdc_existing = cdc_existing.select(*src_cols)
+  cdc_retain = cdc_retain.select(*tgt_cols)
+
+  # Union for final CDC result
+  cdc_result = cdc_updates\
+    .union(cdc_existing)\
+    .union(cdc_retain)
+  return cdc_result
+
+
+
+def job():
+  """ author: Gabriel Hofer """
+  spark = SparkSession.builder.getOrCreate()
+  tgt_df = read_tgt_df(spark)
+  tgt_df.show()
+
+  src_df = spark.createDataFrame(get_data())
+  src_df.show()
+  
+  filtered = filter_by_hospitals_theme(src_df)
+  filtered.show()
+
+  case_converted = cols_to_snake_case(filtered)
+  case_converted.show()
+  
+  new_tgt_df = upsert(tgt_df, case_converted)
+  new_tgt_df.show()
+
+  write_tgt_df(new_tgt_df)
   spark.stop()
 
+
+def main(data_location="data.csv"):
+
+  # get new data every minute
+  #schedule.every(1).minute.at("17:32:00", timezone("America/Chicago")).do(job)
+  schedule.every(10).seconds.at("18:32:00", timezone("America/Chicago")).do(job)
+  while True:
+    schedule.run_pending()
+    time.sleep(1)
+
 #main()
+job()
